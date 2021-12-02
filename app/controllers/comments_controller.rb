90c05b9536ca20167dd8a15e31e98d9d59742087
mozu888 <94517488+mozu888@users.noreply.github.com>
2021-12-02 20:04:26 +0900

    コメントフォームの空送信の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,8 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
+      @prototype = Prototype.find(params[:prototype_id])
+      @comments = @prototype.comments.includes(:user)
       render "prototypes/show"
     end
   end
